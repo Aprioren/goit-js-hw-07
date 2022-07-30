cd1e394f4e21253c9320180bd25c4887748986df
Артем Саранча <98779874+Aprioren@users.noreply.github.com>
2022-07-30 11:45:15 +0300

    working code

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from './gallery-items.js'; //Массив обьектов с картинками
 
-console.log(galleryItems);
+const refs = {  //cсылка на галерею
+    containerRef : document.querySelector('.gallery'),
+}
+
+//Создаем динамически картинки из массива обьектов
+const images = galleryItems.map( e => {  
+    return `<div class="gallery__item">
+       <a class="gallery__link" href="${e.original}">
+         <img
+           class="gallery__image"
+           src="${e.preview}"
+           data-source="${e.original}"
+           alt="${e.description}"
+         />
+       </a>
+     </div>`
+}).join('');
+
+
+refs.containerRef.insertAdjacentHTML('afterbegin', images); //Добавляем разметку в контeинер
+
+refs.containerRef.addEventListener('click', e=>{
+  e.preventDefault();  // запрещаем переход на другую страницу
+
+  const original = e.target.dataset.source; //получаем ссылку на оригинальное изображения для модалки
+
+  if(e.target.nodeName !== 'IMG'){ //клик должен работать только по картинке
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+  <img src="${original}" width="1280" height="600">`);
+  instance.show();
+
+  refs.containerRef.addEventListener('keydown', evt=>{ // проверяем что нажата физическая клавиша Esc
+    if (evt.code === 'Escape'){
+      instance.close();
+    }
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';  //Импорт изображений
+
+const refs = {
+    gallereyRef : document.querySelector('.gallery'), // Ссылка на галерею
+}
+
+const images = galleryItems.map( e => {  // динамическая разметка по массиву картинок
+    return `<div class="gallery__item">
+       <a class="gallery__link" href="${e.original}">
+         <img
+           class="gallery__image"
+           src="${e.preview}"
+           data-source="${e.original}"
+           alt="${e.description}"
+         />
+       </a>
+     </div>`
+}).join('');
+
+refs.gallereyRef.insertAdjacentHTML('afterbegin', images); //добавляем картинки в контеинер
+
+const overlay = images;
+
+let lightbox = new SimpleLightbox('.gallery a', { overlay, images }); //используем библиотеку
+
 
 console.log(galleryItems);
